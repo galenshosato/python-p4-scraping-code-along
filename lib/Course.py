8eb00aa2c90b3c6fa548c7ad89316d8a30e06bb2
Galen Sho Sato <galen.sato@gmail.com>
2023-03-15 10:37:56 -0400

    Completed Lesson

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,13 @@
+
+
 class Course:
-    pass
+    def __init__(self, title='title', schedule='default', description='default'):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+    
+    def __repr__(self):
+        output = ''
+        output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+        output += '-------------------'
+        return output
